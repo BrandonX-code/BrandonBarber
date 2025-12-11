a4a2e9253b9638238e03ea80c67b12b6506e439e
Brandon Daniel Hernandez Fernandez <bhernandez@gasolutions.com.co>
2025-12-11 09:35:13 -0500

    Actualizar modelo y migraciones de base de datos
    Se eliminaron los valores `(string)null` en las definiciones de tablas en `AppDbContextModelSnapshot.cs` para mejorar la claridad.
    Se añadió la columna `ServicioImagen` a la tabla `Citas` con tipo `longtext` y soporte para codificación `utf8mb4`.
    Se actualizaron los archivos de migración (`20251211143415_AddServicioImagenToCitas.cs`) y diseño (`20251211143415_AddServicioImagenToCitas.Designer.cs`) para reflejar estos cambios.
    Se añadieron las propiedades `ServicioImagen`, `ServicioNombre` y `ServicioPrecio` al modelo de la tabla `Citas`.
    Estos cambios mejoran la funcionalidad y mantienen la sincronización entre el modelo y la base de datos.

---
+++ Barber.Maui.API/Migrations/20251211143415_AddServicioImagenToCitas.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Barber.Maui.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddServicioImagenToCitas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ServicioImagen",
+                table: "Citas",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ServicioImagen",
+                table: "Citas");
+        }
+    }
+}
